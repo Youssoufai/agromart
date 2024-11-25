1db25d5b089a183d2c6889bc79c672f9e5c07a2e
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-11-25 19:53:17 +0100

    Why Trade with us section

--- app/components/features.tsx
+++ app/components/features.tsx
@@ -1,3 +1,4 @@
+import { BiWorld } from "react-icons/bi";
 import { CiDeliveryTruck } from "react-icons/ci";
 import { GiFoodChain, GiFoodTruck } from "react-icons/gi";
 
@@ -9,20 +10,20 @@ const Features = () => {
                     <h1 className="font-bold text-3xl">Why Trade <span className="text-color3">With</span>  Us.</h1>
                     <p>Discover how our platform benefits farmers, businesses, and consumers by creating a more efficient and sustainable agricultural ecosystem</p>
                 </div>
-                <section className="m-10 flex">
-                    <div>
+                <section className="m-10 flex gap-4  mt-20">
+                    <div className="text-center flex flex-col items-center space-y-5">
                         <CiDeliveryTruck className="text-8xl text-color3" />
                         <h1 className="text-3xl font-bold">Efficient Logistics</h1>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, laborum. Numquam dolorem magni, in enim iusto nam dignissimos officiis corrupti doloribus, iste nostrum harum a cupiditate consequuntur eos atque? Iure?</p>
                     </div>
-                    <div>
-                        <CiDeliveryTruck className="text-8xl text-color3" />
-                        <h1 className="text-3xl font-bold">Efficient Logistics</h1>
+                    <div className="text-center flex flex-col items-center space-y-5">
+                        <img src="peasant.png" className="text-8xl text-color3 w-[96px] h-[96px]" />
+                        <h1 className="text-3xl font-bold">Market Access to Farmers</h1>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, laborum. Numquam dolorem magni, in enim iusto nam dignissimos officiis corrupti doloribus, iste nostrum harum a cupiditate consequuntur eos atque? Iure?</p>
                     </div>
-                    <div>
-                        <CiDeliveryTruck className="text-8xl text-color3" />
-                        <h1 className="text-3xl font-bold">Efficient Logistics</h1>
+                    <div className="text-center flex flex-col items-center space-y-5">
+                        <img src="supply-chain.png" className="text-8xl  text-color3 w-[96px] h-[96px]" />
+                        <h1 className="text-3xl font-bold">Reliable Supply</h1>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, laborum. Numquam dolorem magni, in enim iusto nam dignissimos officiis corrupti doloribus, iste nostrum harum a cupiditate consequuntur eos atque? Iure?</p>
                     </div>
                 </section>
