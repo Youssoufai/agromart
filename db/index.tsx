6f2c68628d836ce0dde7c5ec5929b0fec96f038b
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-11-29 19:43:57 +0100

    Waitlist Connection

---
+++ db/index.tsx
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+
+let client;
+
+try {
+    if (!process.env.MONGODB_URI) {
+        throw new Error("MongoDB URI is required")
+    }
+    client = new MongoClient(process.env.MONGODB_URI);
+    client.connect();
+} catch (error) {
+    console.log(error);
+}
+export default client;
